948bcffe2c73b2eb379f526d5cbe63e6821733b8
Wei Lin Li <weilinli@Weis-MBP.lan>
2024-07-30 02:46:49 -0400

    completed crud functionalities

--- index.js
+++ index.js
@@ -1,62 +1,101 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Product = require("./model/product.model.js")
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+//import route
 
+//initializing express
 const app = express();
-app.use(express.json());
 
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+app.use("/api/products", productRoutes);
 
 // TEST RES
 app.get("/", (req, res) => {
-    res.send("Hello from node api updated");
+  res.send("Hello from node api updated");
 });
 
 // Connecting to mongo db
-mongoose.connect("mongodb+srv://wlinli2403:ZLhI6smxoFLJxHlo@db1.nhbosge.mongodb.net/?retryWrites=true&w=majority&appName=DB1")
-.then(() => {
+mongoose
+  .connect(
+    "mongodb+srv://wlinli2403:ZLhI6smxoFLJxHlo@db1.nhbosge.mongodb.net/?retryWrites=true&w=majority&appName=DB1"
+  )
+  .then(() => {
     console.log("Connected to database");
-    app.listen(3000, () =>{
-        console.log("Server is running on port 3000");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
     });
-})
-.catch(()=>{
+  })
+  .catch(() => {
     console.log("Connection failed");
-});
+  });
 
 // POST request
-app.post('/api/products', async (req,res) =>{
-    // console.log(req.body);
-    // // res.send("hello");
-    // res.send(req.body);
+app.post("/api/products", async (req, res) => {
+  // console.log(req.body);
+  // // res.send("hello");
+  // res.send(req.body);
 
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-})
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // GET ALL Request
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    }
-    catch(error){
-        res.status(500).json({message: error.message});
-    }
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 //GET individual item
-app.get('/api/products/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.find({id});
-        res.status(200).json(product);
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// PUT
+app.put("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(400).json({ message: "Product not found" });
     }
-    catch (error){
-        res.status(500).json({message : error.message});
+
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// DELETE
+app.delete("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(400).json({ message: "Product not found" });
     }
-})
+
+    res.status(200).json({ message: "Product successfully deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
