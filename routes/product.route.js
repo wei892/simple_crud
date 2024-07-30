d3c5e419c370b8e55b38502ee18ff69f5c6d6f56
wei892 <wlin.li2403@gmail.com>
2024-07-30 03:26:21 -0400

    added comment to test commit

--- routes/product.route.js
+++ routes/product.route.js
@@ -4,7 +4,7 @@ const Product = require("../models/product.model.js");
 //import the controllers
 const {getProducts, getProduct, createProduct, updateProduct, deleteProduct} = require("../controllers/product.controller.js")
 
-
+   //git comment test
 router.get("/", getProducts);
 router.get("/:id", getProduct);
 router.post("/", createProduct);
